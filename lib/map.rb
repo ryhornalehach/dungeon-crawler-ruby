14bdb6b10f214631f137b777a02e80e6fd90adf4
ryhornalehach <ryhornalehach@icloud.com>
2018-01-20 21:41:35 -0500

    fixed move_down

--- lib/map.rb
+++ lib/map.rb
@@ -2,18 +2,27 @@ require 'colorize'
 class Map
 
   LEVEL_1 = [
-          ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', '👾', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', '👳', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', '🍾', ' ', ' ', '#'],
-          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-          ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
+          ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', 'M', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', 'x', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', 'V', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', '#', '#', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', 'V', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+          ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
         ]
 
   def initialize
@@ -25,6 +34,12 @@ class Map
           line.each do |cell|
               if cell == '#'
                   print "#{cell} ".light_black
+              elsif cell == 'M'
+                  print '👾 '      # print monster icon for monsters
+              elsif cell == 'V'
+                  print '🍾 '      # print bottle icon for vials
+              elsif cell == 'x'
+                  print '👳 '      # print bottle icon for player
               else
                   print "#{cell} "
               end
@@ -37,10 +52,10 @@ class Map
       @level.each do |line|
           cell_counter = 0
           line.each do |cell|
-              if cell == '👳'
+              if cell == 'x'
                   if line[cell_counter + 1] != '#'
                     line[cell_counter] = ' '
-                    line[cell_counter + 1] = '👳'
+                    line[cell_counter + 1] = 'x'
                   end
                   break
               end
@@ -53,10 +68,10 @@ class Map
       @level.each do |line|
           cell_counter = 0
           line.each do |cell|
-              if cell == '👳'
+              if cell == 'x'
                   if line[cell_counter - 1] != '#'
                     line[cell_counter] = ' '
-                    line[cell_counter - 1] = '👳'
+                    line[cell_counter - 1] = 'x'
                   end
                   break
               end
@@ -70,10 +85,10 @@ class Map
       @level.each do |line|
           cell_counter = 0
           line.each do |cell|
-              if cell == '👳'
+              if cell == 'x'
                   if @level[line_counter - 1][cell_counter] != '#'
                     @level[line_counter][cell_counter] = ' '
-                    @level[line_counter - 1][cell_counter] = '👳'
+                    @level[line_counter - 1][cell_counter] = 'x'
                   end
                   break
               end
@@ -85,19 +100,22 @@ class Map
 
   def move_down
       line_counter = 0
-      @level.each do |line|
+      do_break = false
+      @level.each do |line|   # searching the map for player location
           cell_counter = 0
           line.each do |cell|
-              if cell == '👳'
-                  if @level[line_counter + 1][cell_counter] != '#'
-                    @level[line_counter][cell_counter] = ' '
-                    @level[line_counter + 1][cell_counter] = '👳'
+              if cell == 'x'
+                  if @level[line_counter + 1][cell_counter] != '#'    # verifying that the destination cell isn't a wall
+                    @level[line_counter][cell_counter] = ' '          # replacing current cell with empty slot
+                    @level[line_counter + 1][cell_counter] = 'x'     # replacing the next cell with player, thus 'moving' the player
                   end
-                  break
+                  do_break = true   # this key will exit the parent loop
+                  break             # breaking the nested loop
               end
               cell_counter += 1
           end
           line_counter += 1
+          break if do_break
       end
   end
 
